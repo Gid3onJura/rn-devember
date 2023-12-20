76c06ba817632348aaa19c9e55f9b744c4a51d7c
Gid3onJura <homebasekorlash@googlemail.com>
2023-12-20 23:29:45 +0100

    edit and preview functionality

--- src/app/(days)/day3/editor.tsx
+++ src/app/(days)/day3/editor.tsx
@@ -1,6 +1,6 @@
 import MarkdownDisplay from "@/components/day3/MarkdownDisplay"
 import React, { useState } from "react"
-import { ScrollView, StyleSheet, Text, View } from "react-native"
+import { Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native"
 import Markdown from "react-native-markdown-display"
 
 const template = `# 🎉 Fun with Markdown!
@@ -13,65 +13,67 @@ Welcome to this **fun and exciting** markdown guide! Let's dive into the world o
 - **Bold and Beautiful:** Make your text stand out! Use \`**\` or \`__\`. Example: **Look at me!**
 - *Sassy Italics:* Add a slant with \`*\` or \`_\`. Example: *I'm leaning!*
 
-### 🍔 Let's List Some Fun Things!
-
-1. 🌟 Star gazing
-2. 🏖 Beach parties
-3. 🍕 Pizza nights
-
-- 🎮 Video games
-- 📚 Reading a good book
-- 🧘 Yoga time
-
-## 🌈 Advanced Fun
-
-### 🖼 Adding Images and Links
-
-A cute pic: 
-
-![Cute Cat](https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/6.jpg)
-
-Visit a fun site: [Fun Site](https://example.com)
-
-### 🎶 Code Block Party
-
-\`\`\`javascript
-// JavaScript party trick
-function partyTime() {
-    console.log("Let's dance 💃🕺!");
-}
-\`\`\`
-
-## 🎤 Conclusion
-Markdown is not just for formatting; it's for having fun while expressing yourself! Keep exploring and enjoy the markdown party! 🎊
-
-> Enjoy crafting your own fun markdown documents! 🎨🎉
 `
 
 const EditorScreen = () => {
   const [content, setContent] = useState(template)
+  const [tab, setTab] = useState("preview")
 
-  return <MarkdownDisplay>{content}</MarkdownDisplay>
+  return (
+    <View style={styles.page}>
+      <View style={styles.tabsContainer}>
+        <Pressable
+          onPress={() => setTab("edit")}
+          style={[styles.tab, { borderColor: tab === "edit" ? "mediumorchid" : "gray" }]}
+        >
+          <Text style={styles.tabText}>Edit</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => setTab("preview")}
+          style={[styles.tab, { borderColor: tab === "preview" ? "mediumorchid" : "gray" }]}
+        >
+          <Text style={styles.tabText}>Preview</Text>
+        </Pressable>
+      </View>
+
+      {tab === "edit" ? (
+        <TextInput value={content} onChangeText={setContent} multiline numberOfLines={1000} style={styles.input} />
+      ) : (
+        <MarkdownDisplay>{content}</MarkdownDisplay>
+      )}
+    </View>
+  )
 }
-
-const markdownStyles = StyleSheet.create({
-  body: {
-    fontFamily: "Roboto",
-  },
-  heading1: {
-    fontSize: 40,
-    fontFamily: "InterSemi",
-  },
-  heading2: {
-    fontFamily: "Inter",
-  },
-})
 
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "whitesmoke",
     padding: 10,
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 20,
+    borderRadius: 10,
+    fontSize: 16,
+    backgroundColor: "white",
+  },
+  tabsContainer: {
+    flexDirection: "row",
+    gap: 10,
+    marginVertical: 10,
+  },
+  tab: {
+    flex: 1,
+    padding: 10,
+    borderColor: "gray",
+    borderWidth: 2,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  tabText: {
+    fontFamily: "Inter",
   },
 })
 
--- src/app/(days)/day3/index.tsx
+++ src/app/(days)/day3/index.tsx
@@ -1,6 +1,8 @@
+import MarkdownDisplay from "@/components/day3/MarkdownDisplay"
 import { Link, Stack } from "expo-router"
 import React from "react"
 import { Button, StyleSheet, Text, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 
 const styles = StyleSheet.create({
   text: {
@@ -10,16 +12,34 @@ const styles = StyleSheet.create({
   },
 })
 
+const description = `
+# Markdown
+
+Integrate Markdown content in **React Native**
+
+📚 Today's Agenda:
+- Introduction to Markdown
+- Markdown Syntax Overview
+- Setting Up React Native for Markdown
+- Implementing Markdown Rendering
+- Styling Markdown Content
+- Using Markdown in React Native Components
+- Recap and Q&A Session
+`
+
 const DayDetailsScreen = () => {
   return (
-    <View>
+    <SafeAreaView edges={["bottom"]} style={{ flex: 1 }}>
       <Stack.Screen options={{ title: "Day 3: Markdown" }} />
+
+      <MarkdownDisplay>{description}</MarkdownDisplay>
+
       <View style={{ marginTop: 20 }}>
         <Link href="/day3/editor" asChild>
           <Button title="Go to markdowneditor" />
         </Link>
       </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
