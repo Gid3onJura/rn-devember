7499417e21acbaaa891e62b2cd631792c6b0f7d7
Gid3onJura <homebasekorlash@googlemail.com>
2023-12-28 19:00:33 +0100

    finish tinder swipe

---
+++ src/app/(days)/day6/TinderScreen.tsx
@@ -0,0 +1,19 @@
+import TinderCard from "@/components/TinderCard"
+import { Stack } from "expo-router"
+import { View } from "react-native"
+import { useSharedValue } from "react-native-reanimated"
+import { users } from "./tinder"
+
+export const TinderScreen = () => {
+  const activeIndex = useSharedValue(0)
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Stack.Screen options={{ headerShown: false }} />
+
+      {users.map((user, index) => (
+        <TinderCard key={user.id} user={user} numOfCards={users.length} index={index} activeIndex={activeIndex} />
+      ))}
+    </View>
+  )
+}
